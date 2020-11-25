7c826cafe809bb55de91696130aa5ff3449c7379
Corne de Jong <corne.dejong@me.com>
2020-11-25 01:29:17 +0100

    refactor of routes, added comments

--- routes/shopping-list.js
+++ routes/shopping-list.js
@@ -6,21 +6,39 @@ module.exports = function (app) {
      * Print Shopping list Endpoint
      */
     app.post('/print/shopping-list', async function (req, res) {
+        /* Create a new printer */
         let printer = new ThermalPrinter(app.printer);
-
+        /* check if we are connected */
         if (!await printer.isPrinterConnected()) {
             res.send('Could not connect to printer!');
             return;
         }
-
+        /* Clone the request body */
         let data = JSON.parse(JSON.stringify(req.body));
+        /* Lets turn the long string of items separated by new lines into an array */
+        /* You could keep the line breaks, works just fine (make sure to update the function below tho) */
+        /* But this way you're able to add some additional line based formatting and add the total items to the end  */
         data.items = data.items.split("\n");
 
-        if (await print(printer, data) !== true) {
-            res.send('An error occurred while printing your shopping list!');
-        } else {
-            res.send('Successfully printed.');
+        try {
+            /* Give the printing a try */
+            await print(printer, data);
+        } catch (error) {
+            /* Shit hit the fan! The printer burned or lost connection mid-print.. we don't know.. */
+            /* This message will be spoken out at the end (when using shortcuts), so be creative */
+            /* You could just change the server logging mesasage to something a bit more informative */
+            let message = 'An error occurred while printing your shopping list!';
+            console.log('✗ ' + message, error);
+            res.send(message);
+            return;
         }
+
+        /* All went well! */
+        /* This message will be spoken out at the end (when using shortcuts), so be creative */
+        /* You could just change the server logging mesasage to something a bit more informative */
+        let message = 'Shopping List Successfully printed!';
+        console.log('✓ ' + message);
+        res.send(message);
     });
 
     /**
@@ -30,10 +48,12 @@ module.exports = function (app) {
     async function print(printer, data) {
         /* Print the logo */
         printer.alignCenter();
-        await printer.printImage("./assets/teunard.png");
+        /* Be creative, add a logo */
+        await printer.printImage("./assets/home-icon-small.png");
         printer.newLine();
-        printer.println("Teunard's Shopping List");
-
+        /* Print the user provided heading */
+        printer.println(data.heading);
+        /* Add a timestamp */
         printer.println("Printed on " + data.requested_at);
         printer.newLine();
 
@@ -43,31 +63,27 @@ module.exports = function (app) {
         printer.newLine();
 
         printer.alignLeft();
+        /* Make the tezt super sized */
         printer.setTextQuadArea();
-
+        /* For each of our shopping list items we're gonna do what? */
         data.items.forEach(function (name) {
-            printer.println("- " + name);
+            /* Print the line and remove all the strange accents that the printer doesn't understand */
+            /* Also, i'd like to prepend them with '-' just for visual purposes */
+            printer.println("- " + name.normalize("NFD").replace(/[\u0300-\u036f]/g, ""));
         });
-
+        /* Draw some separation line */
         printer.setTextSize(0, 0);
         /* Draw some line */
         printer.newLine();
         printer.drawLine();
         printer.newLine();
-
+        /* Let s print the total number of items at the bottom of our list */
         printer.alignCenter();
         printer.println("Total Items: " + data.items.length);
-
+        /* And finaly let s cut the printer */
         printer.cut();
-
-        try {
-            await printer.execute();
-            console.log("Print success.");
-            return true;
-        } catch (error) {
-            console.error("Print error:", error);
-            return error;
-        }
+        /* Lets execute and return */
+        return await printer.execute();
     }
 
 }
--- routes/song-lyrics.js
+++ routes/song-lyrics.js
@@ -12,37 +12,49 @@ module.exports = function (app) {
     app.post('/print/song-lyrics', async function (req, res) {
         /* Setup the printer */
         let printer = new ThermalPrinter(app.printer);
-
         /* Lets check if we're connected */
         if (!await printer.isPrinterConnected()) {
             res.send('Could not connect to printer!');
             return;
         }
-
+        /* Clone the request body */
         let data = JSON.parse(JSON.stringify(req.body));
             
         try {
+            /* Get the lyrics for the provided artist and title from azlyrics */
             data.lyrics = await scraper.getLyric(data.artist + ' - ' + data.title);
         } catch (error) {
-            res.send('Could not find the lyrics to ' + data.artist + '\'s ' + data.title);
+            /* Shit hit the fan! or we could just not find it... probably that */
+            /* This message will be spoken out at the end (when using shortcuts), so be creative */
+            /* You could just change the server logging mesasage to something a bit more informative */
+            let message = 'Could not find the lyrics to ' + data.artist + '\'s ' + data.title;
+            console.log('✗ ' + message, error);
+            res.send(message);
             return;
         }
 
-        let status = true;
-        for (let index = 1; index <= data.times; index++) {
-            status = await print(printer, data);
-            
-            if (status !== true) {
-                res.send('An error occurred while printing the lyrics!');
-                return;
+        try {
+            /* For how many time you'd like to het the lyrics */
+            for (let counter = 1; counter <= data.times; counter++) {
+                /* Give the printing a try */
+                await print(printer, data);
             }
+        } catch (error) {
+            /* Shit hit the fan! The printer burned or lost connection mid-print.. we don't know.. */
+            /* This message will be spoken out at the end (when using shortcuts), so be creative */
+            /* You could just change the server logging mesasage to something a bit more informative */
+            let message = 'An error occurred while printed the lyrics for ' + data.artist + '\'s ' + data.title;
+            console.log('✗ ' + message, error);
+            res.send(message);
+            return;
         }
 
-        if (!status) {
-            res.send('An error occurred while printing the lyrics!');
-        } else {
-            res.send('Successfully printed the lyrics to ' + data.artist + '\'s ' + data.title);
-        }
+        /* All went well! */
+        /* This message will be spoken out at the end (when using shortcuts), so be creative */
+        /* You could just change the server logging mesasage to something a bit more informative */
+        let message = 'Successfully printed the lyrics for ' + data.artist + '\'s ' + data.title;
+        console.log('✓ ' + message);
+        res.send(message);
     });
 
     /**
@@ -52,32 +64,28 @@ module.exports = function (app) {
     async function print(printer, data) {
         /* Print the logo */
         printer.alignCenter();
+        /* Make the tezt super sized */
         printer.setTextQuadArea();
         printer.println(data.artist);
         printer.println(data.title);
-
+        /* Reset the font size to it's default size */
         printer.setTextSize(0, 0);
-        /* Draw some line */
+        /* Draw some separation line */
         printer.newLine();
         printer.drawLine();
-        printer.newLine();
-
+        printer.newLine();  
+        
         printer.alignLeft();
-
+        /* For each of the linse of the lyrics */
         data.lyrics.forEach(function (line) {
+            /* Print the line and remove all the html entities and strange accents that the printer doesn't understand */
+            /* Additionally you could add other stuff here, it's up to you */
             printer.println(entities.decode(line).normalize("NFD").replace(/[\u0300-\u036f]/g, ""));
         }); 
-
+        /* And finaly let s cut the printer */
         printer.cut();
-
-        try {
-            await printer.execute();
-            console.log("Print success.");
-            return true;
-        } catch (error) {
-            console.error("Print error:", error);
-            return error;
-        }
+        /* Lets execute and return */
+        return await printer.execute();
     }
 
 }
